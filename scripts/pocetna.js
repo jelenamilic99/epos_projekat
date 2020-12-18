17bc54aee83349f40d7f13701d10cd03b62f5f4f
Jelena Milic <jelenamilic1999.jm@gmail.com>
2020-12-18 03:38:41 +0100

    dodat jquery popap za voce

--- scripts/pocetna.js
+++ scripts/pocetna.js
@@ -6,25 +6,15 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slajd");
-    var tackice = document.getElementsByClassName("tackica");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < tackice.length; i++) {
-        tackice[i].className = tackice[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    tackice[slideIndex - 1].className += " active";
 }
 
 // U ovoj funkciji pozvati sve one koje treba da se pozovu kad se refresh-uje stranica
---
+++ scripts/voce.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.Voce').magnificPopup({
+        delegate: 'split',
+        type: 'image',
+        closeBtnInside: false,
+        gallery: {
+            enabled: true
+        },
+        closeOnContentClick: true
+    });
+});
+$('.Voce').magnificPopup({
+    delegate: 'split',
+    type: 'image', // other options
+    gallery: {
+        enabled: true
+    },
+    closeBtnInside: false,
+    closeOnContentClick: true
+});
