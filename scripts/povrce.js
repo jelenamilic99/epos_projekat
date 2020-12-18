f03255da1bc16e4d0015b6d20a4036752f34f3c4
Jana Mitrovic <mitrovicjana0@gmail.com>
2020-12-18 11:57:55 +0100

    Povrce jqury

---
+++ scripts/povrce.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.Povrce').magnificPopup({
+        delegate: 'split',
+        type: 'image',
+        closeBtnInside: false,
+        gallery: {
+            enabled: true
+        },
+        closeOnContentClick: true
+    });
+});
+$('.Povrce').magnificPopup({
+    delegate: 'split',
+    type: 'image', // other options
+    gallery: {
+        enabled: true
+    },
+    closeBtnInside: false,
+    closeOnContentClick: true
+});
